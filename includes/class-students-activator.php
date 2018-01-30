b9779941d64f47db9dba8312400b25aa7b4cd7a6
Piotr Gumulka <pjgumulka+github@gmail.com>
2018-01-30 02:36:33 +0100

    create students database

--- includes/class-students-activator.php
+++ includes/class-students-activator.php
@@ -30,7 +30,29 @@ class Students_Activator {
 	 * @since    1.0.0
 	 */
 	public static function activate() {
-
+		global $wpdb;
+		$prefix= $wpdb->prefix;
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+		$table_name= $prefix.'students';
+		$sql = "
+		CREATE TABLE IF NOT EXISTS $table_name (
+			`stuId` int(11) NOT NULL AUTO_INCREMENT,
+		  `stuName` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+		  `stuText` text COLLATE utf8_unicode_ci NOT NULL,
+		  `stuPhoto` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+		  `stuKgs` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+		  `stuCountry` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+		  `stuBirth` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+		  `stuRank` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+		  `stuTripDuration` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+		  `stuGossip` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+		  `stuOrder` tinyint(4) NOT NULL,
+		  `isCurrent` tinyint(4) NOT NULL DEFAULT '1',
+		  `isApproved` tinyint(1) NOT NULL DEFAULT '0',
+			PRIMARY KEY  (stuId)
+		) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+		";
+		dbDelta($sql);
 	}
 
 }
