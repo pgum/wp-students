c7497a20b4b2146f5780d527540ee463c12dadf5
Piotr Gumulka <pjgumulka+github@gmail.com>
2018-02-24 21:16:51 +0100

    activator fixed?

--- includes/class-students-activator.php
+++ includes/class-students-activator.php
@@ -36,19 +36,20 @@ class Students_Activator {
 		$table_name= $prefix.'students';
 		$sql = "
 		CREATE TABLE IF NOT EXISTS $table_name (
-			`stuId` int(11) NOT NULL AUTO_INCREMENT,
-		  `stuName` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-		  `stuText` text COLLATE utf8_unicode_ci NOT NULL,
-		  `stuPhoto` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-		  `stuKgs` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
-		  `stuCountry` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-		  `stuBirth` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
-		  `stuRank` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-		  `stuTripDuration` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-		  `stuGossip` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-		  `stuOrder` tinyint(4) NOT NULL,
-		  `isCurrent` tinyint(4) NOT NULL DEFAULT '1',
-		  `isApproved` tinyint(1) NOT NULL DEFAULT '0',
+			stuId int(11) NOT NULL AUTO_INCREMENT,
+		  stuName varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+		  stuText text COLLATE utf8_unicode_ci NOT NULL,
+		  stuPhoto varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+		  stuKgs varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+		  stuCountry varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+		  stuBirth varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+		  stuRank varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+		  stuTripDuration varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+		  stuGossip varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+		  stuOrder tinyint(4) NOT NULL,
+      prevStuId int(11) NOT NULL,
+		  isCurrent tinyint(4) NOT NULL DEFAULT '1',
+		  isApproved tinyint(1) NOT NULL DEFAULT '0',
 			PRIMARY KEY  (stuId)
 		) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 		";
